9a0d31eddc6a75d2349fb75b85d7f40840718d7e
Aidan Maldonado <aidanm1198@gmail.com>
2019-03-22 11:52:57 -0400

    Initial Commit

---
+++ Billionare/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Billionare
+//
+//  Created by Aidan Maldonado on 3/22/19.
+//  Copyright © 2019 Aidan Maldonado. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
